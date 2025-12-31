e46394499edd8f86ee4ca28afd066f8b37ae473a
AVkings <163132448+AVkings@users.noreply.github.com>
2025-12-31 19:38:25 +0530

    Update from Neura Bilds - main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+# Main Robotics Controller
+
+import time
+
+class Robot:
+    def __init__(self):
+        print("Robot initialized")
+    
+    def move_forward(self):
+        print("Moving forward...")
+
+# Test the robot
+robot = Robot()
+robot.move_forward()
